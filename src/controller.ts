1aac012674525342aebbea9b75fde1d22e8790b6
Tommy <tommyubongabasijohn@gmail.com>
2023-01-28 16:27:34 +0100

    basic redirect setup

--- src/controller.ts
+++ src/controller.ts
@@ -6,13 +6,17 @@ const shorten = async (req: Request, res: Response) => {
     const { destination } = req.body;
     const short = new shortUrl({ destination });
     await short.save();
-    console.log(req);
     res.status(200).send({ link: short.shortUrl });
   } catch (err) {
     console.log(err);
   }
 };
 
-const direct = async (req: Request, res: Response) => {};
+const direct = async (req: Request, res: Response) => {
+  const shortId = req.params.id;
+  const origin = await shortUrl.findOne({ shortId });
+  console.log(origin);
+  if (origin) return res.redirect(`https://${origin?.destination}`);
+};
 
 export { shorten, direct };
