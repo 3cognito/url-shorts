96ca5c48c53eb0e6bb4da04753fdadbdf07d0a99
Tommy <tommyubongabasijohn@gmail.com>
2023-01-29 13:09:53 +0100

    add commet to routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,10 +8,15 @@ import {
 import { Request, Response } from "express";
 
 const routes = (app: Express) => {
+  //To randomly shorten a url
   app.post("/shorten", shorten);
+  //To customise a short
   app.post("/customize", customize);
+  //To edit a url - can only edit a customised short
   app.patch("/edit", editShort);
+  //Redirect
   app.get("/:id", direct);
+  //Home page
   app.get("/", async (req: Request, res: Response) => {
     res.send("Home page");
   });
