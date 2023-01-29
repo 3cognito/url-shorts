c75365a58c62e9e9434b41e08c138adae4188a1e
Tommy <tommyubongabasijohn@gmail.com>
2023-01-29 13:08:08 +0100

    edit endpoint - only for customised shorts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,16 @@
 import { Express } from "express-serve-static-core";
-import { customize, direct, shorten } from "../controllers/controller";
+import {
+  customize,
+  direct,
+  shorten,
+  editShort,
+} from "../controllers/controller";
 import { Request, Response } from "express";
 
 const routes = (app: Express) => {
   app.post("/shorten", shorten);
   app.post("/customize", customize);
+  app.patch("/edit", editShort);
   app.get("/:id", direct);
   app.get("/", async (req: Request, res: Response) => {
     res.send("Home page");
