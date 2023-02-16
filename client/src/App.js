37ea1aaaae5ad96142593220a8928e916c07e73d
Bobbiie <mmeyenendaeyo@gmail.com>
2023-02-16 10:53:58 +0100

    Feat: I added a POST request for customizing url. Fix: I made some style fixes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from 'react';
+import { React, useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import axios from 'axios';
 import './App.css';
@@ -7,31 +7,34 @@ function App() {
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
   const [custom, setCustom] = useState("");
+  const [longmessage, setLongMessage] = useState("");
+  const [customMessage, setCustomMessage] = useState("");
   const [loading, setLoading] = useState(false)
   const [copied, setCopied] = useState(false)
   const [disable, setDisable] = useState(true)
-  const [longmessage, setLongMessage] = useState("");
-  const [customMessage, setCustomMessage] = useState("");
 
+  // Consuming endpoint for url shortening
   const fetchData = async () => {
-    try {
-      const res = await axios.post("http://127.0.0.1:5000/shorten", {destination: input});
-      setTimeout(() => {
+    // Customized
+    if(custom.length >= 5){
+      try {
+        const res = await axios.post("http://127.0.0.1:5000/customize", {destination: input, custom: custom});
         setResult("http://127.0.0.1:5000/"+res.data.link)
-      }, "2000");
-    } catch (error) {
-      console.log(error)
+      } catch (error) {
+        setCustomMessage(error.response.data.message);
+        setCustom(JSON.parse(error.config.data).custom)
+        setInput(JSON.parse(error.config.data).destination)
+      }
+    } else {
+    // Random
+      try {
+        const res = await axios.post("http://127.0.0.1:5000/shorten", {destination: input});
+        setResult("http://127.0.0.1:5000/"+res.data.link)
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
-
-  // Resetting values of error messages
-  useEffect(() => {
-    setTimeout(() => setLongMessage(""), "5000")
-  }, [longmessage]);
-
-  useEffect(() => {
-    setTimeout(() => setCustomMessage(""), "5000")
-  }, [customMessage]);
 
   const handleClick = () => {
     if(input){
@@ -43,29 +46,29 @@ function App() {
         input.includes("https://")
       ) {
         setInput("");
+        setCustom("");
         setLoading(true);
         setTimeout(() => {
           setLoading(false);
-        }, "2500");
+        }, "500");
 
         setLongMessage("");
         fetchData();
       } else setLongMessage("Input a valid url");
       
-    } else if (input === ''){
+    } else if (input === '' || (custom.length <= 5 && custom.length !== 0)){
       setLongMessage('Field cannot stay empty');
-    } else if (input.includes('http') || input.includes('https') || input.includes('https://')){
-      setLongMessage('Input a valid url')
+      setDisable(true);
     }
   }
 
   return (
     <div className="app">
+      {loading ? <p className="loading-text">shortening...</p> : null}
       <h1 className={loading ? "heading-text loading" : "heading-text"}>
         url-shorts
       </h1>
       {/* Input container */}
-      {loading ? <p className="loading-text">shortening...</p> : null}
       <div className={loading ? "input-container loading" : "input-container"}>
         <div className="inputs">
           <div className={longmessage ? "long-url_input error" : "long-url_input"}>
@@ -80,8 +83,10 @@ function App() {
                   e.target.value.includes("www.")
                 ) {
                   setLongMessage("");
+                  setDisable(false);
                 } else {
                   setLongMessage("Input a valid url");
+                  setDisable(true);
                 }
               }}
             />
@@ -95,10 +100,12 @@ function App() {
               value={custom}
               onChange={(e) => {
                 setCustom(e.target.value)
-                if(e.target.value.length < 7){
-                  setCustomMessage("URL title must be at least 7 characters long")
+                if(e.target.value.length < 5 && e.target.value.length !== 0){
+                  setCustomMessage("URL title must be at least 5 characters long")
+                  setDisable(true);
                 } else {
                   setCustomMessage("")
+                  setDisable(false)
                 }
               }}
             />
@@ -107,7 +114,7 @@ function App() {
           </div>
         </div>
 
-        <button onClick={handleClick} type="submit">
+        <button onClick={handleClick} type="submit" disabled={disable}>
           shorten
         </button>
       </div>
@@ -121,20 +128,18 @@ function App() {
             setResult(e.target.value);
             setCopied(false);
           }}
-          disabled={disable}
+          disabled={true}
         />
-        {disable ? (
-          <div className="result-container_buttons">
-            <CopyToClipboard text={result} onCopy={() => setCopied(true)}>
-              <button
-                onClick={() => setTimeout(() => setCopied(false), "2000")}
-                title="Copy"
-              >
-                📎
-              </button>
-            </CopyToClipboard>
-          </div>
-        ) : undefined}
+        <div className="result-container_buttons">
+          <CopyToClipboard text={result} onCopy={() => setCopied(true)}>
+            <button
+              onClick={() => setTimeout(() => setCopied(false), "2000")}
+              title="Copy"
+            >
+              📎
+            </button>
+          </CopyToClipboard>
+        </div>
         <div className="copied">{copied ? <p>URL copied!</p> : null}</div>
       </div>
     </div>
